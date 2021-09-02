a0c123c8894f94e389d4b90d591ba0ef0022adb0
daniellembryant <danbry08@yahoo.com>
2021-09-02 18:48:38 -0500

    Updates movies boolean to defualt to true

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_02_000710) do
+ActiveRecord::Schema.define(version: 2021_09_02_234327) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,7 +32,7 @@ ActiveRecord::Schema.define(version: 2021_09_02_000710) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "director"
-    t.boolean "english"
+    t.boolean "english", default: true
   end
 
 end
